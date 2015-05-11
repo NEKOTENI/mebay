203489666d039afae53f429db738e4606eb75e17
NEKOTENI <riverfield.tk@gmail.com>
2015-05-11 16:29:25 +0900

    new file index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507073334) do
+ActiveRecord::Schema.define(version: 20081127162635) do
 
   create_table "ads", force: true do |t|
     t.string   "name"
